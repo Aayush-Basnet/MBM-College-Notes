6f1c9f5ca32d0c5430cc7ebaecf75fd66a69504d
Aayush Basnet <122598100+Aayush-Basnet@users.noreply.github.com>
2023-01-22 18:32:51 +0545

    Multiplication Tapble using Loop
    Just enter a number you want for multiplication table. It will give you output upto  n*10=N.

---
+++ multiplicationtable.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+
+int i,n,t1;
+
+printf("Enter any number: ");
+scanf("%d",&n);
+printf("Multiplication Table of %d\n",n);
+for(i=1;i<=10;i++){
+        t1=n*i;
+    printf("%d*%d=%d \n",n,i,t1);
+
+
+
+
+
+
+}
+
+
+
+}
