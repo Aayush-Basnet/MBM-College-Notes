98b671deb73ba473add522ff9c933f3e83edaf51
Aayush Basnet <aayushbasnet531@gmail.com>
2024-02-18 15:40:40 +0545

    Add files via upload

---
+++ TOC_Codes/regular_expression1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int table[3][2] = {{1, 0}, {1, 2}, {1, 0}};
+    int i, l, status = 0;
+
+    char input[100];
+
+    printf("To implement DFA of language (a+b)*ab\nEnter Input String: ");
+    scanf("%s", input);
+
+    l = strlen(input);
+
+    for (i = 0; i < l; i++) {
+        if (input[i] != 'a' && input[i] != 'b') {
+            printf("The entered value is wrong.\n");
+            return 0;
+        }
+
+        if (input[i] == 'a') {
+            status = table[status][0];
+        } else {
+            status = table[status][1];
+        }
+    }
+
+    if (status == 2) {
+        printf("String Accepted\n");
+    } else {
+        printf("String not Accepted\n");
+    }
+
+    return 0;
+}
